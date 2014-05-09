1be1e7af80515738b4cc36fd294861044d203b4e
Takashi Sato <takashi@tks.st>
2014-05-09 21:11:00 +0900

    インデントを浅くするための変更。 no functional change.

--- httpd-translation-status/test.js
+++ httpd-translation-status/test.js
@@ -61,31 +61,35 @@ function moveVersion(ver){
 	var req = new XMLHttpRequest();
 	req.open('GET', ver + ".json", true);
 	req.onreadystatechange = function(){
-		if (req.readyState == 4 && req.status == 200){
-			removeAllChild(e);
-			var tb = ce("tbody", ce("table", e));
-			var obj = JSON.parse(req.responseText);
-			
-			//ヘッダ
-			{
-				var tr = ce("tr", tb);
-				ce("th", tr);
-				for(var i = 0; i < obj["langs"].length; ++i){
-					 ce("th", tr).innerHTML = obj["langs"][i];
-				}
+		if(req.readyState != 4){
+			return;
+		}
+		if(req.status != 200){
+			return;
+		}
+		removeAllChild(e);
+		var tb = ce("tbody", ce("table", e));
+		var obj = JSON.parse(req.responseText);
+		
+		//ヘッダ
+		{
+			var tr = ce("tr", tb);
+			ce("th", tr);
+			for(var i = 0; i < obj["langs"].length; ++i){
+				 ce("th", tr).innerHTML = obj["langs"][i];
 			}
-			//ボディ
-			for(var i = 0; i < obj["files"].length; ++i){
-				var tr = ce("tr", tb);
-				//ファイル名
-				var file = obj["files"][i];
-				ctd(tr, file["filename"]).className = "filename";
-				//各言語
-				for(var j = 0; j < obj["langs"].length; ++j){
-					var lang = obj["langs"][j];
-					var c = className(file[lang], file["en"], lang);
-					ctd(tr, keta(file[lang])).className = c;
-				}
+		}
+		//ボディ
+		for(var i = 0; i < obj["files"].length; ++i){
+			var tr = ce("tr", tb);
+			//ファイル名
+			var file = obj["files"][i];
+			ctd(tr, file["filename"]).className = "filename";
+			//各言語
+			for(var j = 0; j < obj["langs"].length; ++j){
+				var lang = obj["langs"][j];
+				var c = className(file[lang], file["en"], lang);
+				ctd(tr, keta(file[lang])).className = c;
 			}
 		}
 	};
