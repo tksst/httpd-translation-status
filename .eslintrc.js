deb17d46c4f645d90b922264451708df1a0645d1
Takashi Sato <takashi@tks.st>
2019-05-04 16:04:49 +0900

    ESLint & Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "./tsconfig.json",
+        "tsconfigRootDir": "."
+    },
+    "env": {
+        "browser": true
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react",
+        "prettier",
+    ],
+    "extends": [
+        "airbnb",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+        "prettier/@typescript-eslint"
+    ],
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    },
+    "rules": {
+        "react/jsx-filename-extension": [
+            "error",
+            {
+                "extensions": [
+                    ".jsx",
+                    ".tsx"
+                ]
+            }
+        ],
+        // strange behavior, conflict with prettier?
+        "react/jsx-indent": "off",
+
+        "react/prop-types": "off",
+        "react/jsx-key": "warn",
+        "no-param-reassign": "off",
+        "no-prototype-builtins": "warn",
+        "prefer-template": "warn",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+
+        "prettier/prettier": [
+            "error",
+            {
+                "printWidth": 120,
+                "tabWidth": 4,
+                "trailingComma": "es5",
+                "endOfLine": "lf",
+            }
+        ],
+    }
+}
